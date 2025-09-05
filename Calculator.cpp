da728cff1b67875209d8c1f384e59a1b850b8e45
aman280 <74920319+aman280@users.noreply.github.com>
2025-09-05 14:58:12 +0530

    Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,58 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class Calculator {
-public:
-    void menu() {
-        cout << " AMAN Calculator" << endl;
-        cout << "1. Addition of numbers" << endl;
-        cout << "2. Subtraction of numbers" << endl;
-        cout << "3. Multiplication of numbers" << endl;
-        cout << "4. Division of numbers" << endl;
-        cout << "5. Modulus of numbers" << endl;
-        cout << "6. Exit from the menu" << endl;
-    }
-
-    void performOperation(int choice) {
-        int a, b;
-        if (choice >= 1 && choice <= 5) {
-            cout << "Enter your First no.: ";
-            cin >> a;
-            cout << "Enter your Second no.: ";
-            cin >> b;
-        }
-
-        switch (choice) {
-            case 1: cout << "Your Result: " << (a + b) << endl; break;
-            case 2: cout << "Your Result: " << (a - b) << endl; break;
-            case 3: cout << "Your Result: " << (a * b) << endl; break;
-            case 4: 
-                if (b != 0)
-                    cout << " Your Result: " << (a / b) << endl;
-                else
-                    cout << "Error bro  have you not read in primary classes." << endl;
-                break;
-            case 5: 
-                if (b != 0)
-                    cout << " Your Result: " << (a % b) << endl;
-                else
-                    cout << "error bro have you not read in primary classes " << endl;
-                break;
-            case 6: cout << "Exiting..." << endl; break;
-            default: cout << "Invalid choice!" << endl;
-        }
-    }
-};
-
 int main() {
-    Calculator calc;
-    int choice;
-    do {
-        calc.menu();
-        cout << "Enter your choice: ";
-        cin >> choice;
-        calc.performOperation(choice);
+    double a, b;
+    float n;
+
+    cout << "How many times do you want to calculate? ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << "Calculation " << i << ":" << endl;
+
+        cout << "Enter first digit : ";
+        cin >> a;
+        cout << "Enter second digit: ";
+        cin >> b;
+
+        cout << "Addition of numbers: " << a + b << endl;
+        cout << "Subtraction of numbers: " << a - b << endl;
+        cout << "Multiplication of numbers: " << a * b << endl;
+
+        if (b != 0)
+            cout << "Divisionof numbers: " << a / b << endl;
+        else
+            cout << "A number canot be divided by zero (itna v nahi pata bhai)" << endl;
+
+        if (b != 0)
+            cout << "you will get the remainder(modulus): " << a % b << endl;
+        else
+            cout << "Error bro you can get modulus by dividing zero" << endl;
+
         cout << endl;
-    } while (choice != 6);
+    }
+
+    cout << "Exit from the Calculator." << endl;
     return 0;
 }
